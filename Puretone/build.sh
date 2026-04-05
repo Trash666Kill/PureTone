5982d7186f89e8081887484e54fb96a0d92067cb
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2026-04-05 10:52:25 -0300

    update

--- Puretone/build.sh
+++ Puretone/build.sh
@@ -1,7 +1,7 @@
 # Dependências de sistema
-sudo apt install \
-    gcc ccache \
-    python3 python3-dev python3-pip
+sudo apt install gcc ccache build-essential \
+    python3 python3-dev python3-pip \
+    libpython3-dev python3-venv
 
 # venv
 python3 -m venv venv
@@ -9,3 +9,7 @@ source venv/bin/activate
 
 # Nuitka e zstandard (compressão do onefile) no usuário
 pip install nuitka zstandard --break-system-packages
+
+
+
+build-essential libpython3-dev python3-venv
